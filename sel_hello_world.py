fc274d178195fa7b0b04f527ac8dcf7a5e83c14c
samliangsk <qliang@ucsb.edu>
2023-02-21 16:16:16 -0800

    added arguments that support headless mode

--- sel_hello_world.py
+++ sel_hello_world.py
@@ -7,11 +7,15 @@ import sys
 import re
 import subprocess
 
-from pyvirtualdisplay.display import Display
+# from pyvirtualdisplay.display import Display
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
 
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(options=options)
 driver.get('http://www.google.com')
 print(driver.title)
 driver.close()
